ba452f17f8bf0c613d95c59657ad1bb33df1fe8f
Daniel-Modilevsky <62332613+Daniel-Modilevsky@users.noreply.github.com>
2021-09-13 13:06:51 +0300

    timeline left

--- src/modules/requests/request.tsx
+++ src/modules/requests/request.tsx
@@ -1,9 +1,8 @@
-import React , {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Request } from "./request.iterface";
 import RequestTag from "../../elements/tag";
 import { Timeline } from "antd";
 import "antd/dist/antd.css";
-
 
 type SingleRequestProps = {
   request: Request;
@@ -20,34 +19,70 @@ const SingleRequest: React.FC<SingleRequestProps> = (props) => {
   const { selectedRequest } = props;
   const { index } = props;
 
-  useEffect(() => {
-  }, [selectedRequest]);
+  useEffect(() => {}, [selectedRequest]);
 
   const timeWithotYear = date.substring(0, date.length - 4);
   const timeWithotDate = time.substring(10, time.length);
-  let backGroundColor:string = "white" || "#edf0f9";
+  let backGroundColor: string = "white" || "#edf0f9";
 
-  let custumPath:string;
+  let custumPath: string;
 
-  if(request.path.includes("2fb")){
+  if (request.path.includes("2fb")) {
     const indexStart = request.path.indexOf("2fb");
     custumPath = request.path.slice(0, indexStart) + "user_id";
-  } else{
+  } else {
     custumPath = request.path;
   }
 
-  if(selectedRequest === index){
+  if (selectedRequest === index) {
     backGroundColor = "#edf0f9";
-  } else{
+  } else {
     backGroundColor = "white";
   }
 
   return (
-    <>       
-        <Timeline.Item label={dayOn ?  (<span style={{display:"inline-block", backgroundColor:"#ddd" , height: 40, padding: 10, width:90, marginLeft:-100}}>
-        {timeWithotYear}
-        </span>) : (<span style={{display:"inline-block", height: 40, padding: 10, width:90}}>
-        </span>)} style={{backgroundColor:backGroundColor ,marginLeft:100, display:"block", height:60, paddingTop:-10}}>{timeWithotDate}{" "}<RequestTag method={request.method} />{" "}{custumPath}</Timeline.Item>
+    <>
+      <Timeline.Item
+        label={
+          dayOn ? (
+            <p
+              style={{
+                display: "inline-block",
+                backgroundColor: "#ddd",
+                height: 40,
+                padding: 10,
+                width: 90,
+                marginLeft: -100,
+              }}
+            >
+              {timeWithotYear}
+            </p>
+          ) : (
+            <p
+              style={{
+                display: "inline-block",
+                height: 40,
+                padding: 10,
+                width: 90,
+                marginLeft: -100,
+              }}
+            >
+              {timeWithotDate}
+            </p>
+          )
+        }
+        style={{
+          backgroundColor: backGroundColor,
+          marginLeft: 100,
+          display: "flex",
+          height: 60,
+        }}
+      >
+        {" "}
+        <p style={{ marginTop: 20 }}>
+          <RequestTag method={request.method} /> {custumPath}
+        </p>
+      </Timeline.Item>
     </>
   );
 };
